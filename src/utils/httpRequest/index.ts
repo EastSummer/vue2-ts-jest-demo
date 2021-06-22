f488a85a78216e38691ab17181e8ffced81509a3
JokerGhy <1533521853@qq.com>
2021-06-22 17:33:45 +0800

    update: axios完善

--- src/utils/httpRequest/index.ts
+++ src/utils/httpRequest/index.ts
@@ -1,19 +1,17 @@
 import { Interceptors } from './interceptors'
-import { AxiosPromise, AxiosInstance } from 'axios'
+import { AxiosPromise, AxiosInstance, AxiosResponse } from 'axios'
 
-export class httpRequest{
+class HttpRequest{
   axios: any
-  promise: AxiosPromise | null
   constructor() {
     this.axios = new Interceptors().getAxios()
-    this.promise = null
   }
-  sendRequest(config: any): AxiosPromise | null {
-    if (!config.method) {
-      this.promise = this.axios.post(config)
-    } else {
-      this.promise = this.axios[config.method](config)
-    }
-    return this.promise
+  request(config: any): AxiosPromise | null {
+    return new Promise((resolve, reject) => {
+        this.axios.post(config).then((res: AxiosResponse) => {}).catch((error: any) => {})
+      })
   }
 }
+
+const httpRequest = new HttpRequest()
+export default httpRequest

3ae645ac3f03ba002444c72e8c2742b7de76cea6
JokerGhy <1533521853@qq.com>
2021-06-22 17:41:42 +0800

    update: axios更新

--- src/utils/httpRequest/index.ts
+++ src/utils/httpRequest/index.ts
@@ -1,14 +1,14 @@
 import { Interceptors } from './interceptors'
-import { AxiosPromise, AxiosInstance, AxiosResponse } from 'axios'
+import { AxiosPromise, AxiosResponse } from 'axios'
 
 class HttpRequest{
   axios: any
   constructor() {
     this.axios = new Interceptors().getAxios()
   }
-  request(config: any): AxiosPromise | null {
+  request(config: any): AxiosPromise {
     return new Promise((resolve, reject) => {
-        this.axios.post(config).then((res: AxiosResponse) => {}).catch((error: any) => {})
+        this.axios.post(config).then((res: AxiosResponse) => { resolve(res)}).catch((error: any) => { reject(error)})
       })
   }
 }
